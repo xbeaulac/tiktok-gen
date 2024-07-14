d9f6ecf319e4e59ed1d6f24ca0c087c3706bb9c9
Xander Beaulac <xbeaulac@gmail.com>
2024-07-14 14:54:49 -0700

    added height, width, fps, and duration to get_reddit_posts

--- scripts/get_reddit_posts.py
+++ scripts/get_reddit_posts.py
@@ -1,6 +1,7 @@
 import requests
 from requests.exceptions import HTTPError, Timeout, RequestException
 import time
+import m3u8
 
 
 def fetch_url(url, retries=3, delay=5):
@@ -45,19 +46,35 @@ def get_reddit_posts(subreddit: str, after: str = ""):
             title: str = post['data']['title'].replace(".", "").replace(",", "").replace('"', '').strip()
             if is_video:
                 video_url: str = post['data']['secure_media']['reddit_video']['fallback_url'].split('?')[0]
+
+                height = post['data']['secure_media']['reddit_video']['height']
+                width = post['data']['secure_media']['reddit_video']['width']
+
+                m3u8_url = post['data']['secure_media']['reddit_video']['hls_url']
+                m3u8_obj = m3u8.load(m3u8_url)
+                stream_info = str(m3u8_obj.playlists[0].stream_info).split(',')
+                fps = int([info.split('=')[1] for info in stream_info if "FRAME-RATE" in info][0])
+
                 audio_url = video_url.split('/')[:-1]
                 audio_url.append('DASH_AUDIO_128.mp4')
                 audio_url = '/'.join(audio_url)
                 media_url = {'video': video_url, 'audio': audio_url}
+
                 duration = int(post['data']['secure_media']['reddit_video']['duration'])
             else:
+                height = post['data']['preview']['images'][0]['source']['height']
+                width = post['data']['preview']['images'][0]['source']['width']
                 media_url = post['data']['url_overridden_by_dest']
                 duration = 0
+                fps = 0
             post_json = {
                 'title': title,
                 'url': media_url,
                 'type': 'video' if is_video else 'image',
-                'duration': duration
+                'duration': duration,
+                'fps': fps,
+                'height': height,
+                'width': width
             }
 
             posts.append(post_json)
