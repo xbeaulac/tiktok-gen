ec5a2fa7e2592db2badf200cba3a93db356536da
Xander Beaulac <xbeaulac@gmail.com>
2024-07-15 08:08:06 -0700

    made get_song_url headless even though I thought it already was

--- scripts/get_song_url.py
+++ scripts/get_song_url.py
@@ -3,8 +3,9 @@ from playwright.sync_api import sync_playwright
 
 
 def get_headers(playwright):
-    browser = playwright.chromium.launch(headless=False, timeout=(10 * 1000))
-    context = browser.new_context()
+    browser = playwright.chromium.launch(headless=True, timeout=(10 * 1000))
+    context = browser.new_context(
+        user_agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
     page = context.new_page()
 
     request_sent = False
@@ -45,7 +46,6 @@ def get_song_url():
     url = "https://ads.tiktok.com/creative_radar_api/v1/popular_trend/sound/rank_list?period=7&page=1&limit=10&rank_type=popular&new_on_board=false&commercial_music=false&country_code=US"
     response = requests.get(url, headers=headers)
     data = response.json()
-    print(data)
     song_id = data['data']['sound_list'][0]['clip_id']
 
     url = f"https://www.tiktok.com/api/music/item_list/?aid=1988&count=2&cursor=0&musicID={song_id}"
